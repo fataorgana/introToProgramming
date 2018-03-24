564325da8da80a28f638804fade7fcbe128b6a44
Michael Burns <M.Burns@2016.ljmu.ac.uk>
2018-03-24 10:33:34 +0000

    Tutorial 1
    Drawing simple shapes by printing to the console

---
+++ Tutorial01/src/tutorail01/Shapes.java
@@ -0,0 +1,28 @@
+package tutorail01;
+
+public class Shapes {
+
+	public static void main(String[] args) {
+
+		System.out.println("   *****");
+		System.out.println("   *   *");
+		System.out.println("   *****\n");
+		
+		System.out.println("    * *");
+		System.out.println("  *     *");
+		System.out.println(" *       *");
+		System.out.println(" *       *");
+		System.out.println("  *     *");
+		System.out.println("    * *\n");
+		
+		System.out.println("     *");
+		System.out.println("    ***");
+		System.out.println("   *****");
+		System.out.println("     *");
+		System.out.println("     *");
+		System.out.println("     *");
+		System.out.println("     *");
+		
+		
+	}
+}
